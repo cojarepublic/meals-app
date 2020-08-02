150d78a8a0a43d426a0c67eb8402c6e7d782f736
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-08-02 19:52:59 +0100

    A meals app that shows details of a particular meal type

---
+++ lib/categories_meals_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/meal.dart';
+import 'package:mealsapp/meal_item.dart';
+import 'dummy_data.dart';
+
+class CategoriesMealsScreen extends StatefulWidget {
+  static const routeName = '/categories-meals';
+  final List<Meal> availableMeals;
+
+  CategoriesMealsScreen(this.availableMeals);
+
+  @override
+  _CategoriesMealsScreenState createState() => _CategoriesMealsScreenState();
+}
+
+class _CategoriesMealsScreenState extends State<CategoriesMealsScreen> {
+  String categoryTitle;
+  List displayedMeals;
+
+
+  @override
+  void didChangeDependencies() {
+    final routeArgs =
+    ModalRoute.of(context).settings.arguments as Map<String, String>;
+    categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    displayedMeals = widget.availableMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+
+  void initState() {
+    super.initState();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+              id: displayedMeals[index].id,
+              title: displayedMeals[index].title,
+              imageUrl: displayedMeals[index].imageUrl,
+              duration: displayedMeals[index].duration,
+              affordability: displayedMeals[index].affordability,
+              complexity: displayedMeals[index].complexity,
+            );
+          },
+          itemCount: displayedMeals.length,
+        ));
+  }
+}
